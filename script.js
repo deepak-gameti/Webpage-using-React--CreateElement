27445c8b7ac1fa02b224b81b4ff029a1cf8a3864
deepak-gameti <159270881+deepak-gameti@users.noreply.github.com>
2024-05-13 22:15:46 +0530

    push React create element project

---
+++ script.js
@@ -0,0 +1,504 @@
+const webpage = React.createElement("main", { key: 1, style: {} }, [
+  React.createElement(
+    "nav",
+    {
+      key: 1,
+      style: {
+        height: "60px",
+        borderBottom: "1px solid #d4d4d4a2",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: "0 20rem",
+        background: "black",
+        color: "white",
+      },
+    },
+    [
+      React.createElement(
+        "div",
+        {
+          key: 1,
+          style: {
+            fontSize: "2rem",
+            fontWeight: "600",
+            letterSpacing: ".2rem",
+          },
+        },
+        "LOGO"
+      ),
+      React.createElement(
+        "ul",
+        {
+          key: 2,
+          style: {
+            display: "flex",
+            listStyle: "none",
+            gap: "2rem",
+            fontSize: "1.2rem",
+          },
+        },
+        [
+          React.createElement(
+            "li",
+            { key: 1, style: { cursor: "pointer" } },
+            "Home"
+          ),
+          React.createElement(
+            "li",
+            { key: 2, style: { cursor: "pointer" } },
+            "Product"
+          ),
+          React.createElement(
+            "li",
+            { key: 3, style: { cursor: "pointer" } },
+            "Servies"
+          ),
+          React.createElement(
+            "li",
+            { key: 4, style: { cursor: "pointer" } },
+            "Contact"
+          ),
+        ]
+      ),
+    ]
+  ),
+  React.createElement(
+    "header",
+    {
+      key: 2,
+      style: {
+        height: "calc(100vh)",
+        width: "100%",
+        background: "url('assets/header.jpg') no-repeat center",
+        backgroundSize: "cover",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      },
+    },
+    [
+      React.createElement(
+        "div",
+        { key: 1, style: { textAlign: "center", lineHeight: "6rem" } },
+        [
+          React.createElement(
+            "h2",
+            {
+              key: 1,
+              style: { fontSize: "7rem", fontWeight: "300", color: "white" },
+            },
+            "Dramatically Engage"
+          ),
+          React.createElement(
+            "p",
+            {
+              key: 2,
+              style: {
+                fontSize: "1.2rem",
+                fontWeight: "300",
+                color: "#d4d4d4a2",
+              },
+            },
+            "Objectively innovate empowered manufactured products whereas parallel platforms."
+          ),
+          React.createElement(
+            "button",
+            {
+              key: 3,
+              style: {
+                border: "none",
+                height: "50px",
+                width: "180px",
+                padding: ".5rem",
+                fontSize: "1.2rem",
+                fontWeight: "500",
+                borderRadius: "3rem",
+                background: "#9adb03",
+                color: "white",
+              },
+            },
+            "ENGAGE NOW"
+          ),
+        ]
+      ),
+    ]
+  ),
+  React.createElement(
+    "div",
+    {
+      key: 3,
+      style: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        textAlign: "center",
+        padding: "5rem 10rem",
+        background: "#9adb03",
+        color: "white",
+      },
+    },
+    [
+      React.createElement(
+        "h2",
+        {
+          key: 1,
+          style: { marginBottom: "1rem", fontSize: "2rem", fontWeight: "500" },
+        },
+        "Completely synergize resource taxing relationships"
+      ),
+      React.createElement(
+        "p",
+        { key: 2, style: { width: "800px" } },
+        "Professionally cultivate one-to-one customer service with robust ideas. Dynamically innovate resource-leveling customer service for state of the art customer service."
+      ),
+    ]
+  ),
+  React.createElement(
+    "div",
+    {
+      key: 4,
+      style: {
+        color: "black",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "2rem",
+        height: "600px",
+      },
+    },
+    [
+      React.createElement(
+        "div",
+        {
+          key: 1,
+          style: {
+            height: "380px",
+            width: "550px",
+            background: "url('assets/microphone.jpg') no-repeat center",
+            borderRadius: "50%",
+          },
+        },
+        ""
+      ),
+      React.createElement(
+        "div",
+        { key: 2, style: { textAlign: "left", width: "30%" } },
+        [
+          React.createElement(
+            "h2",
+            { key: 1, style: { fontSize: "2rem", fontWeight: "400" } },
+            "Best in Class"
+          ),
+          React.createElement(
+            "p",
+            {
+              key: 2,
+              style: {
+                fontSize: "1.2rem",
+                fontWeight: "200",
+                marginBottom: "1rem",
+                alignSelf: "",
+              },
+            },
+            "Holisticly predominate extensible testing procedures for reliable supply chains. Dynamically innovate resource-leveling customer service for state of the art customer service."
+          ),
+          React.createElement(
+            "button",
+            {
+              key: 3,
+              style: {
+                border: "none",
+                height: "50px",
+                width: "180px",
+                padding: ".5rem",
+                fontSize: "1.2rem",
+                fontWeight: "500",
+                borderRadius: "3rem",
+                background: "#9adb03",
+                color: "white",
+              },
+            },
+            "ENGAGE NOW"
+          ),
+        ]
+      ),
+    ]
+  ),
+
+  React.createElement(
+    "div",
+    {
+      key: 5,
+      style: {
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "2rem",
+        height: "600px",
+        background: "#1b1b1b",
+      },
+    },
+    [
+      React.createElement(
+        "div",
+        { key: 2, style: { textAlign: "left", width: "30%" } },
+        [
+          React.createElement(
+            "h2",
+            { key: 1, style: { fontSize: "2rem", fontWeight: "400" } },
+            "Superior Quality"
+          ),
+          React.createElement(
+            "p",
+            {
+              key: 2,
+              style: {
+                fontSize: "1.2rem",
+                fontWeight: "200",
+                marginBottom: "1rem",
+                alignSelf: "",
+              },
+            },
+            "Holisticly predominate extensible testing procedures for reliable supply chains. Dynamically innovate resource-leveling customer service for state of the art customer service."
+          ),
+          React.createElement(
+            "button",
+            {
+              key: 3,
+              style: {
+                border: "none",
+                height: "60px",
+                width: "140px",
+                padding: ".5rem",
+                fontSize: "1.2rem",
+                fontWeight: "500",
+                borderRadius: "3rem",
+                background: "#00000",
+                color: "black",
+              },
+            },
+            "TEST IT"
+          ),
+        ]
+      ),
+      React.createElement(
+        "div",
+        {
+          key: 1,
+          style: {
+            height: "380px",
+            width: "550px",
+            background: "url('assets/iphone.jpg') no-repeat center",
+            borderRadius: "50%",
+          },
+        },
+        ""
+      ),
+    ]
+  ),
+
+  React.createElement(
+    "div",
+    {
+      key: 6,
+      style: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+      },
+    },
+    [
+      React.createElement(
+        "div",
+        {
+          key: 1,
+          style: {
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            background: "url('assets/writing.jpg') no-repeat center",
+            height: "200px",
+            width: "33.33%",
+          },
+        },
+        [
+          React.createElement(
+            "span",
+            { key: 1, style: { color: "white", fontSize: "2.5rem" } },
+            "Unleash"
+          ),
+        ]
+      ),
+      React.createElement(
+        "div",
+        {
+          key: 2,
+          style: {
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            background: "url('assets/concert.jpg') no-repeat center",
+            height: "200px",
+            width: "33.33%",
+          },
+        },
+        [
+          React.createElement(
+            "span",
+            { key: 1, style: { color: "white", fontSize: "2.5rem" } },
+            "Synergize"
+          ),
+        ]
+      ),
+      React.createElement(
+        "div",
+        {
+          key: 3,
+          style: {
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            background: "url('assets/pencil_sharpener.jpg') no-repeat center",
+            height: "200px",
+            width: "33.33%",
+          },
+        },
+        [
+          React.createElement(
+            "span",
+            { key: 1, style: { color: "white", fontSize: "2.5rem" } },
+            "Procrastinate"
+          ),
+        ]
+      ),
+    ]
+  ),
+
+  React.createElement(
+    "div",
+    {
+      key: 3,
+      style: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        textAlign: "center",
+        padding: "5rem 10rem",
+        background: "white",
+        color: "black",
+      },
+    },
+    [
+      React.createElement(
+        "h2",
+        {
+          key: 1,
+          style: { marginBottom: "1rem", fontSize: "2rem", fontWeight: "500" },
+        },
+        "Sanity Check"
+      ),
+      React.createElement(
+        "p",
+        {
+          key: 2,
+          style: { width: "800px", color: "gray", marginBottom: "1rem" },
+        },
+        "Holisticly predominate extensible testing procedures for reliable supply chains. Dynamically innovate resource-leveling customer service for state of the art customer service."
+      ),
+      React.createElement(
+        "button",
+        {
+          key: 3,
+          style: {
+            border: "none",
+            height: "50px",
+            width: "180px",
+            padding: ".5rem",
+            fontSize: "1.2rem",
+            fontWeight: "500",
+            borderRadius: "3rem",
+            background: "#9adb03",
+            color: "white",
+          },
+        },
+        "CHECK NOW"
+      ),
+    ]
+  ),
+
+  React.createElement(
+    "div",
+    {
+      key: 3,
+      style: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        textAlign: "center",
+        padding: "5rem 10rem",
+        background: "#9adb03",
+        color: "white",
+      },
+    },
+    [
+      React.createElement(
+        "h2",
+        {
+          key: 1,
+          style: { marginBottom: "1rem", fontSize: "2rem", fontWeight: "500" },
+        },
+        "Contact Us"
+      ),
+      React.createElement(
+        "p",
+        {
+          key: 2,
+          style: { width: "800px", fontSize: "1.5rem", marginBottom: "1rem" },
+        },
+        "+1(23) 456 7890"
+      ),
+      React.createElement(
+        "p",
+        {
+          key: 2,
+          style: { width: "800px", fontSize: "1.5rem", marginBottom: "1rem" },
+        },
+        "info@example.com"
+      ),
+    ]
+  ),
+
+  React.createElement(
+    "div",
+    {
+      key: 3,
+      style: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        textAlign: "center",
+        padding: "2rem 10rem",
+        background: "white",
+        color: "black",
+      },
+    },
+    [
+      React.createElement(
+        "h2",
+        { key: 1, style: { fontSize: "1rem", fontWeight: "300" } },
+        "Copyright © Example.com 2015"
+      ),
+    ]
+  ),
+]);
+
+const element = document.querySelector("#root");
+const root = ReactDOM.createRoot(element);
+root.render(webpage);
